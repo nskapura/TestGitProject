99a2889f185f846789820110369d2d4dd3084ac0
nskapura <nskapura@premierdocs.com>
2013-01-16 14:52:04 -0500

    Initial commit.

---
+++ TestGitProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestGitProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a simple test project.");
+        }
+    }
+}
